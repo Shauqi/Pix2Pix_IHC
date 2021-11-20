d95358d8909c695920b72877d360dc0a545777ec
mhasan <mahmudulhasanshauqi@gmail.com>
2021-11-19 20:28:24 -0500

    19_November_2021

--- models/base_model.py
+++ models/base_model.py
@@ -85,6 +85,7 @@ class BaseModel(ABC):
             self.schedulers = [networks.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
         if not self.isTrain or opt.continue_train:
             load_suffix = 'iter_%d' % opt.load_iter if opt.load_iter > 0 else opt.epoch
+            print(load_suffix)
             self.load_networks(load_suffix)
         self.print_networks(opt.verbose)
 
